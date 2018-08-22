fae2c039717054e1202bb551b33cbb54785df668
Chandrasekar Perumal <chandrasekar.vlsi@gmail.com>
2018-08-22 19:52:59 +0530

    Add existing file

---
+++ examples/BME280/README.md
@@ -0,0 +1,6 @@
+# nrf52_BME280_SPI
+
+nRF52840 SPI Interface with SPI
+
+BME280 driver files are used from https://github.com/BoschSensortec/BME280_driver
+ 
---
+++ examples/BME280/pca10040/s132/armgcc/_build/nrf52832_xxaa_app_util_platform.c.d
@@ -0,0 +1,149 @@
+_build/nrf52832_xxaa_app_util_platform.c.o: \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_util_platform.c \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_util_platform.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/compiler_abstraction.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf52.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cm4.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cmInstr.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cmFunc.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cmSimd.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/system_nrf52.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf52_bitfields.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf51_to_nrf52.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf52_name_change.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/compiler_abstraction.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_soc.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_svc.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_error_soc.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_error.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_nvic.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/nrf_assert.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_error.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdio.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/newlib.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/config.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/ieeefp.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/cdefs.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/reent.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/_ansi.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_types.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_types.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/lock.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/types.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/types.h \
+ /usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/stdio.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/sdk_errors.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_error.h \
+ ../config/sdk_config.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/nordic_common.h \
+ ../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_error_weak.h
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_util_platform.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdint.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_default_types.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/features.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_intsup.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_stdint.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/compiler_abstraction.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf52.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cm4.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cmInstr.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/cmsis_gcc.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cmFunc.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/cmsis/include/core_cmSimd.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/toolchain/system_nrf52.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf52_bitfields.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf51_to_nrf52.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/nrf52_name_change.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/device/compiler_abstraction.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_soc.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdbool.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_svc.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_error_soc.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_error.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_nvic.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/nrf_assert.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_error.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/stdio.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/newlib.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/config.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/ieeefp.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/cdefs.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stddef.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/lib/gcc/arm-none-eabi/4.9.3/include/stdarg.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/reent.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/_ansi.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/_types.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/_types.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/lock.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/types.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/machine/types.h:
+
+/usr/local/gcc-arm-none-eabi-4_9-2015q3/arm-none-eabi/include/sys/stdio.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/sdk_errors.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/softdevice/s132/headers/nrf_error.h:
+
+../config/sdk_config.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/nordic_common.h:
+
+../../../../../nRF5_SDK_12.3.0_d7731ad//components/libraries/util/app_error_weak.h:
